bebfa15911a128c83f7d20e23219bbb24ea4cc6c
shaganga <iamshankar.g@gmail.com>
2023-06-29 14:19:02 +0530

    Create after-install.sh

---
+++ scripts/after-install.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Change to the application directory
+cd /var/www/html/node-demo
+
+# Install application dependencies
+npm install
+
+# Build the application (if required)
+# npm run build
